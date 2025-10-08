47eefb27ae532791d2210f2e472296d9e69aee5f
trikmgithub <trikmforwork@gmail.com>
2025-10-08 10:31:56 +0100

    Add .gitignore

---
+++ src/main/java/com/trikmgithub/test/automation/selenium/web/driver/TestAutomationSeleniumWebDriver.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.trikmgithub.test.automation.selenium.web.driver;
+
+/**
+ *
+ * @author Admin
+ */
+public class TestAutomationSeleniumWebDriver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
